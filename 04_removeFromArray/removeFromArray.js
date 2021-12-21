be464a4de66c5b3dedb067b3af031af943f9dd8f
Jarrell Mikko Romero <jarrellmikkoromero@gmail.com>
2021-12-22 00:49:24 +0800

    Perform exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,34 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, elem1, 
+                            elem2, elem3, elem4) {
 
+    for(let i = 0; i < array.length; i++){
+        if(elem1 === array[i]){
+            const spliced = array.splice(i, 1);           
+        }
+    }
+
+    for(let i = 0; i < array.length; i++){
+
+        if(elem2 === array[i]){
+            const spliced = array.splice(i, 1);
+        }
+    }
+
+    for(let i = 0; i < array.length; i++){
+
+        if(elem3 === array[i]){
+            const spliced = array.splice(i, 1);
+        }
+    }
+    
+    for(let i = 0; i < array.length; i++){
+
+        if(elem4 === array[i]){
+            const spliced = array.splice(i, 1);
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    if(number1 < 0 || number2 < 0 || 
+        typeof number1 != typeof NaN|| 
+        typeof number2 != typeof NaN){
+        return 'ERROR';
+    }
 
+    if(number1 < number2){
+        let sum = number1;
+        for (let i = number1; i < number2; i++){
+            let nextNum = i + 1;
+            sum = sum + nextNum;
+        }
+        return sum;
+    }
+    else if(number1 > number2){
+        let sum = number1;
+        for (let i = number1; i > number2; i--){
+            let nextNum = i - 1;
+            sum = sum + nextNum;
+        }
+        return sum;
+    }
+    
+    
 };
 
 // Do not edit below this line
