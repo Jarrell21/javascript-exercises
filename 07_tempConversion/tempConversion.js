084a37f63535b12dfbbb23dfe38b25322ebc7cd6
Jarrell Mikko Romero <jarrellmikkoromero@gmail.com>
2021-12-22 18:24:11 +0800

    Perform exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  const celsius = (farenheit -32)*(5/9);
+  const roundedCelsius = Math.round(celsius * 10) / 10;
+  return roundedCelsius;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  const farenheit = celsius*(9/5)+32;
+  const roundedFarenheit = Math.round(farenheit * 10) / 10;
+  return roundedFarenheit;
 };
 
 // Do not edit below this line
