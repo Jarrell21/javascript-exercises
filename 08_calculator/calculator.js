02446faed457d62b39be8a9349308a42761b5bf8
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2021-12-31 20:49:27 +0800

    Perform exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	array.forEach(num => sum+=num);
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let product = 1;
+	array.forEach(num => product*=num);
+  return product;
+};
+
+const power = function(a, b) {
+	return Math.pow(a, b);
+};
+
+const factorial = function(num) {
+  let total = 1;
+	for(let i = num; i > 0; i--){
+    total*=i;
+  }
+  return total;
+  
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
