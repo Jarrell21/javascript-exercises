caa9634e0dd20cb929f308cb252ff5a283aac2c8
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2022-01-02 00:50:14 +0800

    Perform exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const strLowerCase = str.toLowerCase();
+    const strNoSymbols = strLowerCase.replace(/!/g, '').replace(/ /g, '')
+                            .replace(/,/g, '').replace('.', '');
+    const strToArray = strNoSymbols.split('');
+    const reverseArrayToString = strToArray.reverse().join('');
+    if(strNoSymbols === reverseArrayToString){
+        return true;
+    }
+    return false;
 
 };
 
