bc46b56f9f7b80394282fbee884649c159adaddd
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2022-01-02 09:02:32 +0800

    Perform exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0) return 'OOPS';
+    if(num === 0) return 0;
+    if(num == 1 || num == 2) return 1;
+    let currentNum = 1;
+    let nextNum = 1;
+    let sum = 1;
+    let fiboArray = [currentNum, nextNum];
+    for(let i = 0; i < num-2; i++){
+        currentNum = nextNum;
+        nextNum = sum;
+        sum = currentNum+nextNum;
+        fiboArray.push(sum);
+    }
 
+    return fiboArray[num-1]; 
 };
 
 // Do not edit below this line
